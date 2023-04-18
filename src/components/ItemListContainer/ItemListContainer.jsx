79199ed8a2d24ab32c90a0f8e247721a114403a1
fabianlopez <lettofabi8@gmail.com>
2023-04-17 23:11:28 -0300

    otro commit

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import './ItemListContainer.css'
+import { useState,useEffect  } from 'react'4.2k (gzipped: 1.8k)
+import {getProducts} from '../../asyncMock'
+import ItemList from '../ItemList/ItemList'
 
-const ItemListContainer = (props) => {
-  return (
+import './ItemListContainer.css'
+import React from 'react'
+const ItemListContainer = ({greeting}) => {
+  const[products,setProducts]= useState([])
+  useEffect(() => {
+    getProducts()
+     .then(response => {
+        setProducts(response)
+  })
+    .catch(error=> {
+        console.error(error)
+    })
+},[] )   
    
-      <h2 className='subtitulo' >{props.greeting} </h2>
+   return (
+     <div>
+        <h1>{greeting} </h1>
+        
+     </div>
     
   )
 }
